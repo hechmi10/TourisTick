b3bb51bcc072646a5449537371e44d40dc51dd2f
hechmi10 <hechmi.jaidane@gmail.com>
2024-12-11 08:21:05 +0100

    Finaliser l'application

--- app/src/main/java/tn/esprit/touristick/views/ForgotPasswordActivity.kt
+++ app/src/main/java/tn/esprit/touristick/views/ForgotPasswordActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import tn.esprit.touristick.controllers.TouristController
 import tn.esprit.touristick.databinding.ActivityForgotPasswordBinding
 import tn.esprit.touristick.models.Tourist
@@ -13,18 +14,19 @@ const val NEWPASSWORD="New_Password"
 class ForgotPasswordActivity : AppCompatActivity() {
     private lateinit var binding:ActivityForgotPasswordBinding
     private lateinit var controller:TouristController
+    private lateinit var firebaseAuth:FirebaseAuth
 
     override fun onCreate(savedInstanceState:Bundle?) {
         super.onCreate(savedInstanceState)
         binding=ActivityForgotPasswordBinding.inflate(layoutInflater)
         setContentView(binding.root)
         controller=TouristController.getInstance()
+        firebaseAuth=FirebaseAuth.getInstance()
         val cin=intent.getStringExtra(CIN)
         val nom=intent.getStringExtra(NOM_TOURISTE)
         val prenom=intent.getStringExtra(PRENOM_TOURISTE)
         val email=intent.getStringExtra(EMAIL)
         binding.btnSetPassword.setOnClickListener {
-            setPassword()
             if (binding.etOldPassword.text.toString()
                     .isBlank() || binding.etNewPassword.text.toString().isBlank()
             ) {
@@ -39,18 +41,20 @@ class ForgotPasswordActivity : AppCompatActivity() {
                         binding.etNewPassword.text.toString()
                     ) , this
                 )
-                val intent=Intent(this , ReservationManagementActivity::class.java).apply {
-                    putExtra(NOM_TOURISTE , nom)
-                    putExtra(PRENOM_TOURISTE , prenom)
-                    putExtra(CIN , cin)
-                    putExtra(EMAIL , email)
-                }
-                startActivity(intent)
+                firebaseAuth.confirmPasswordReset(binding.etOldPassword.text.toString(),
+                    binding.etNewPassword.text.toString()).addOnCompleteListener { task->
+                        if(task.isSuccessful){
+                            val intent=Intent(this , ReservationManagementActivity::class.java).apply {
+                                putExtra(NOM_TOURISTE , nom)
+                                putExtra(PRENOM_TOURISTE , prenom)
+                                putExtra(CIN , cin)
+                                putExtra(EMAIL , email)
+                                putExtra(MOT_DE_PASSE,binding.etNewPassword.text.toString())
+                            }
+                            startActivity(intent)
+                        }
+                    }
             }
         }
-    }
-
-    private fun setPassword() {
-
     }
 }
