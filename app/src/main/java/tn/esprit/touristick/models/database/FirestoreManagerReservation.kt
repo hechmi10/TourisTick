e801a45e9673562691a9a74a6667854e50ce2e55
Jaidene Hachemi <hechmi.jaidane@gmail.com>
2024-12-20 09:56:21 +0100

    Rendre le nom du réservation unique

--- app/src/main/java/tn/esprit/touristick/models/database/FirestoreManagerReservation.kt
+++ app/src/main/java/tn/esprit/touristick/models/database/FirestoreManagerReservation.kt
@@ -124,15 +124,15 @@ class FirestoreManagerReservation {
             .whereEqualTo("nom" , nom)
             .get()
             .addOnSuccessListener { querySnapshot ->
-                val document=querySnapshot.documents.firstOrNull()
-                    document?.reference?.delete()?.addOnSuccessListener {
+                val document=querySnapshot.documents.first()
+                    document.reference.delete().addOnSuccessListener {
                         Log.d(TAG , "Reservation deleted successfully!")
                         Toast.makeText(
                             context ,
                             "Reservation deleted successfully!" ,
                             Toast.LENGTH_SHORT
                         ).show()
-                    }?.addOnFailureListener {
+                    }.addOnFailureListener {
                         Log.e(TAG , "Failed to delete reservation: $it")
                         Toast.makeText(
                             context ,
@@ -143,7 +143,7 @@ class FirestoreManagerReservation {
                 }
             .addOnFailureListener {
                 Log.e(TAG , "Error fetching reservation: $it")
-                Toast.makeText(context , "Failed to delete reservation!" , Toast.LENGTH_SHORT)
+                Toast.makeText(context , "No reservation found with this name!" , Toast.LENGTH_SHORT)
                     .show()
             }
     }
